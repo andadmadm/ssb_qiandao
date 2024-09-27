61b56e72b70e9ed60f0e570a6df81632187674d0
andadmadm <58729839+andadmadm@users.noreply.github.com>
2024-09-27 21:41:20 +0800

    Update ssb_url.py

--- ssb_url.py
+++ ssb_url.py
@@ -4,7 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 import logging
-import time  # 确保导入 time 模块
+import time
 
 # 设置日志记录
 logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 def get_refresh_url(url: str):
     try:
         response = requests.get(url)
-        if response.status_code != 403:
+        if response.status_code != 200:
             response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -29,37 +29,29 @@ def get_refresh_url(url: str):
             print("No meta refresh tag found.")
             return None
     except Exception as e:
-        print(f'An unexpected error occurred: {e}')
+        logger.error(f'An unexpected error occurred: {e}')
         return None
 
 def get_url(url: str):
-    try:
-        resp = requests.get(url)
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        
-        links = soup.find_all('a', href=True)
-        for link in links:
-            if link.text == "搜书吧":
-                return link['href']
-    except Exception as e:
-        print(f'Error fetching URL: {e}')
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    
+    links = soup.find_all('a', href=True)
+    for link in links:
+        if link.text == "搜书吧":
+            return link['href']
     return None
 
 if __name__ == '__main__':
     try:
         redirect_url = get_refresh_url('http://' + os.environ.get('kvasd.dpkd.5asfws6fpm.com', 'www.soushu2025.com'))
         time.sleep(2)
-        
-        if redirect_url:  # 确保 redirect_url 不为 None
+        if redirect_url:
             redirect_url2 = get_refresh_url(redirect_url)
-            url = get_url(redirect_url2)
-            if url:  # 确保 url 不为 None
+            if redirect_url2:
+                url = get_url(redirect_url2)
                 with open('ssb.txt', 'w', encoding='utf-8') as f:
                     f.write(f'{url}\n')
                 logger.info(f'{url}')
-            else:
-                logger.warning("No URL found.")
-        else:
-            logger.warning("No redirect URL found.")
     except Exception as e:
-        logger.error(f'An unexpected error occurred in main: {e}')
+        logger.error(f'Error in main execution: {e}')
