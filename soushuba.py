740cebecd7cb625005c8416f8aae71c3e3b52035
andadmadm <58729839+andadmadm@users.noreply.github.com>
2024-09-27 21:25:39 +0800

    Update soushuba.py

--- soushuba.py
+++ soushuba.py
@@ -152,19 +152,18 @@ class SouShuBaClient:
 if __name__ == '__main__':
     try:
         redirect_url = get_refresh_url('http://' + os.environ.get('kvasd.dpkd.5asfws6fpm.com', 'www.soushu2025.com'))
-        with open('ssb.txt', 'w', encoding='utf-8') as f:
-            f.write(f'{redirect_url}\n')
         time.sleep(2)
         redirect_url2 = get_refresh_url(redirect_url)
         url = get_url(redirect_url2)
         logger.info(f'{url}')
-        client = SouShuBaClient(urlparse(url).hostname,
-                                os.environ.get('SOUSHUBA_USERNAME', "libesse"),
-                                os.environ.get('SOUSHUBA_PASSWORD', "yF9pnSBLH3wpnLd"))
-        client.login()
-        client.space()
-        credit = client.credit()
-        logger.info(f'{client.username} have {credit} coins!')
-    except Exception as e:
-        logger.error(e)
+        
+        parsed_url = urlparse(url)
+        ssb_url_domain = parsed_url.hostname
+        
+        # 将 ssb-url 的域名部分写入 ssb.txt，并格式化 payload
+        with open('ssb.txt', 'w', encoding='utf-8') as f:
+            f.write('payload:\n')
+            f.write(f'  - "+.{ssb_url_domain}"\n')
+
+        
         sys.exit(1)
